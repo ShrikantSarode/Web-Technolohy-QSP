4ed15c808b9c04e2068c6ac7a783fb8fea6ce0e6
Shrikant <srikantsarode786@gmail.com>
2024-10-14 23:07:26 +0530

    all wt codes

---
+++ JS/typecasting.js
@@ -0,0 +1,35 @@
+console.log(10+10);
+console.log(10+"10");
+console.log("a"-1);
+console.log("10"-5);
+console.log("10"-"10");
+console.log(true+"true");
+console.log(true+10);
+console.log(false-5);
+console.log(true+true);
+console.log(false-5);
+console.log(true+true);
+console.log(5+"true");
+console.log(null-true);
+console.log(""-true);
+console.log(" "-true);
+console.log(" "+true);
+console.log(true-" ");
+console.log(true-"a");
+console.log('a'+10);
+console.log("33"+"33"-"3");
+console.log("33"+"33"+"4"-"3"+"33"-"3");
+console.log("33"-"33"+"3");
+console.log("33"+"33"-33);
+
+
+///Explicit TypeCasting
+console.log(Number("10")+10);
+console.log(true+Boolean("String"));
+console.log(String(true)-true);
+console.log(Boolean(2)+false);
+console.log(Number(true)+"33");
+console.log(Number(null)+null);
+console.log(Boolean("Kattapa"));
+console.log("10"+10);
+
